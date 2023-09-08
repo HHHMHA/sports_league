bf73ffc75a9d953689f28ee9f5257f8f1789b8de
J2mF <mp4-12cs5@hotmail.com>
2023-09-08 11:13:27 +0300

    feat(games): add more tests for services

--- sports_league/sports/tests/test_services.py
+++ sports_league/sports/tests/test_services.py
@@ -68,3 +68,40 @@ class TestGameService:
         assert team2.loses == 0
         assert team2.draws == 0
         assert team2.games_count == 0
+
+    def test_update_team_statistics__with_create_game(self):
+        team1 = Team.objects.create(name="Team A")
+        team2 = Team.objects.create(name="Team B")
+
+        game = Game.objects.create(home_team=team1, home_team_score=2, away_team=team2, away_team_score=1)
+        GameService.update_team_statistics(game)
+
+        team1.refresh_from_db()
+        team2.refresh_from_db()
+        assert team1.wins == 1
+        assert team1.loses == 0
+        assert team1.draws == 0
+        assert team1.games_count == 1
+        assert team2.wins == 0
+        assert team2.loses == 1
+        assert team2.draws == 0
+        assert team2.games_count == 1
+
+    def test_update_team_statistics__delete_game(self):
+        team1 = Team.objects.create(name="Team A")
+        team2 = Team.objects.create(name="Team B")
+        game = GameService.create_game("Team A", 2, "Team B", 1)
+        game.delete()
+
+        GameService.update_team_statistics(game, delete=True)
+
+        team1.refresh_from_db()
+        team2.refresh_from_db()
+        assert team1.wins == 0
+        assert team1.loses == 0
+        assert team1.draws == 0
+        assert team1.games_count == 0
+        assert team2.wins == 0
+        assert team2.loses == 0
+        assert team2.draws == 0
+        assert team2.games_count == 0
